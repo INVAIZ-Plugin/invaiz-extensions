0c39c601470db0cd201cc470f911bedc5c30126c
ChangjunPark <ckdwns9121@naver.com>
2022-04-20 18:00:23 +0900

    # 코드사인 자동화

--- codesign.bat
+++ codesign.bat
@@ -1,2 +1,2 @@
 ZXPSignCmd -selfSignedCert KR Busan INVAIZ Changjundev jun9121 INVAIZCert.p12
-ZXPSignCmd -sign com.invaiz.extension com.invaiz.extension.zxp INVAIZCert.p12 jun9121
+ZXPSignCmd -sign dist com.invaiz.extension.zxp INVAIZCert.p12 jun9121
