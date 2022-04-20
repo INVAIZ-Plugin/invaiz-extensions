5a298a0900ef7698aeb00a4494dd6ec401b97b2d
ChangjunPark <ckdwns9121@naver.com>
2022-04-20 17:53:45 +0900

    # 배쉬파일 업데이트

--- codesign.bat
+++ codesign.bat
@@ -1,2 +1,2 @@
-ZXPSignCmd -selfSignedCert KR VA Invaiz Changjundev jun9121 InvaizCert.p12
-ZXPSignCmd -sign com.invaiz.extension com.invaiz.extension.zxp InvaizCert.p12 jun9121
+ZXPSignCmd -selfSignedCert KR Busan INVAIZ Changjundev jun9121 INVAIZCert.p12
+ZXPSignCmd -sign com.invaiz.extension com.invaiz.extension.zxp INVAIZCert.p12 jun9121
