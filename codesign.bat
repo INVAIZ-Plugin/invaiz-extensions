e7e21e15360fe7bee1a43708dc7522722e5e5383
ChangjunPark <ckdwns9121@naver.com>
2022-04-21 14:36:33 +0900

    # 압축 해제

--- codesign.bat
+++ codesign.bat
@@ -1,4 +1,5 @@
 ZXPSignCmd -selfSignedCert KR Busan INVAIZ Changjundev jun9121 INVAIZCert.p12
 ZXPSignCmd -sign dist com.invaiz.extension.zxp INVAIZCert.p12 jun9121
 rename com.invaiz.extension.zxp com.invaiz.extension.zip
-unzip com.invaiz.extension.zip
+mkdir com.invaiz.extension
+tar -zxvf com.invaiz.extension.zip -C com.invaiz.extension
